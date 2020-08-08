41d844481f95489a319f52b636b74d4c9a7fadf3
Student_GA126 <student_ga126@psxga126.lan>
2020-08-08 08:15:01 -0400

    dotenv install

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 
+// Used to hide environmental variables within code
+require('dotenv').config();
+
 function App() {
   return (
     <div className="App">
