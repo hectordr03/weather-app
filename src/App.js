3d20c7409d70a180898bb3b5469bd5f2ee9e6100
Student_GA126 <student_ga126@psxga126.lan>
2020-08-08 09:56:48 -0400

    reaching data

--- src/App.js
+++ src/App.js
@@ -5,16 +5,15 @@ import axios from 'axios';
 // Used to hide environmental variables within code
 require('dotenv').config();
 
-// const NYC_ID = 5128581;
-// const BASE_URL = `api.openweathermap.org/data/2.5/forecast?id=${NYC_ID}&appid=${process.env.API_KEY}`;
-const UNSAFE_URL = `HTTPS://api.openweathermap.org/data/2.5/forecast?q=new york city&appid=69a82df7ee6726e168a96d57b2eeaf2e`
+const cityName = "new york city";
+// const BASE_URL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${process.env.API_KEY}`;
+const UNSAFE_URL = `HTTPS://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=69a82df7ee6726e168a96d57b2eeaf2e`
 
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      info: [],
     }
   }
 
@@ -23,10 +22,11 @@ class App extends React.Component {
     .then(res => (
       this.setState({
         info: res.data,
+        list: res.data.list,
       })
     ))
     .catch(e => console.error(e.message));
-    console.log(this.state.info)
+    console.log(this.state.list[0].main)
   }
   
   render() {
